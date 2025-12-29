63e2dd65a8be145dde55b56b36a490cf7724c29e
Jack Koppa <hello@jackkoppa.com>
2025-12-29 13:19:57 -0500

    chore: Fix script dir

--- start-beads-ui.sh
+++ start-beads-ui.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 # Convenience script to start Beads UI for browse-dot-show
 
-cd /Users/jackkoppa/Personal_Development/browse-dot-show
+# Get the directory where this script is located
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+cd "$SCRIPT_DIR"
+
 pnpm exec bdui start --open
