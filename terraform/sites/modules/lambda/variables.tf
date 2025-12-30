328b97e06ecc1700aa929d1ccc7edc79ba41d917
Jack Koppa <hello@jackkoppa.com>
2025-12-30 12:34:54 -0500

    Upgrade Lambda runtime from Node 20.x to Node 24.x
    AWS Lambda is deprecating Node 20.x runtime support. This commit upgrades all Lambda functions and related configurations to use Node 24.x runtime.
    
    Changes:
    - Update all Lambda function runtimes from nodejs20.x to nodejs24.x in Terraform
    - Update Lambda layer compatible_runtimes to nodejs24.x
    - Update .nvmrc from 20 to 24
    - Update package.json engines to require Node >=24.0.0
    - Update @types/node dependencies from ^20.0.0 to ^24.0.0
    - Update Lambda layer README with Node 24 version for nodelayer.xyz
    - Update documentation references from Node 20 to Node 24
    
    Reference: https://docs.aws.amazon.com/lambda/latest/dg/lambda-nodejs.html#nodejs-sdk-included

--- terraform/sites/modules/lambda/variables.tf
+++ terraform/sites/modules/lambda/variables.tf
@@ -11,7 +11,7 @@ variable "handler" {
 variable "runtime" {
   description = "The runtime for the Lambda function"
   type        = string
-  default     = "nodejs20.x"
+  default     = "nodejs24.x"
 }
 
 variable "memory_size" {
